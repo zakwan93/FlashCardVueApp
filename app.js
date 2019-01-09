aa7a0dbb9748547305c193e5137df30a7963f8af
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-01-09 13:55:55 -0500

    started with basic setup of app

---
+++ app.js
@@ -0,0 +1,33 @@
+const cards = [
+    {
+      front: 'The "First Computer Programmer"',
+      back: 'Ada Lovelace',
+      flipped: false,
+    },
+    {
+      front: 'Invented the "Clarke Calculator"',
+      back: 'Edith Clarke',
+      flipped: false,
+  
+    },
+    {
+      front: 'Famous World War II Enigma code breaker',
+      back: 'Alan Turing',
+      flipped: false,
+    },
+    {
+      front: 'Created satellite orbit analyzation software for NASA',
+      back: 'Dr. Evelyn Boyd Granville',
+      flipped: false,
+    },
+  ]; 
+
+  new Vue({
+    el: '#flashcard-app',
+    data: {
+      cards: cards,
+    },
+    methods: {
+
+    }
+  });
